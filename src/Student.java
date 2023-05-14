f607500722961497fa74b1d0404eca66b0e49003
SuryajaChedge <130437044+SuryajaChedge@users.noreply.github.com>
2023-05-14 13:39:32 +0530

    First commit

---
+++ src/Student.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Student
+{
+    private JTextField txtStName;
+    private JTextField txtMarks1;
+    private JTextField txtMarks2;
+    private JTextField txtMarks3;
+    private JTextField txtTotal;
+    private JTextField txtAverage;
+    private JTextField txtGrade;
+    private JButton calculateButton;
+    private JPanel Main;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Student");
+        frame.setContentPane(new Student().Main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public Student()
+    {
+        calculateButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                int m1, m2, m3, total;
+                double avg;
+
+                m1= Integer.parseInt(txtMarks1.getText());
+                m2= Integer.parseInt(txtMarks2.getText());
+                m3= Integer.parseInt(txtMarks3.getText());
+
+                total= m1+m2+m3;
+                txtTotal.setText(String.valueOf(total));
+
+                avg= total/3f;
+                txtAverage.setText(String.valueOf(avg));
+
+                if(avg>50)
+                {
+                    txtGrade.setText("Pass");
+                }
+                else
+                {
+                    txtGrade.setText("Fail");
+                }
+            }
+        });
+    }
+}
